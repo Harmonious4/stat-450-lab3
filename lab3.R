0d626a4435b948b0fb1dfade30995fd42bddb7db
Harmonious4 <nlmacc4b@gmail.com>
2025-01-24 15:48:08 -0800

    added lab3.R (step 2)

---
+++ lab3.R
@@ -0,0 +1,14 @@
+# Run this to load all necessary libraries
+library(tidyverse)
+# install.packages("palmerpenguins") # Run this if you can't load the palmerpenguins library 
+library(palmerpenguins)
+pgs <- penguins %>% drop_na
+head(pgs)
+
+pgs %>% 
+  ggplot() +
+  geom_point(aes(x = bill_length_mm, y = bill_depth_mm, color = species),
+             size = 2, alpha = 0.6) +
+  labs(x = "Bill length (mm)",
+       y = "Bill depth (mm)",
+       color = "Species")
